cd2f0401fd62946ebb8001bf2093fc12a8307dee
Michelle <choi.michelle.12@gmail.com>
2023-01-14 11:49:40 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let cards = []
+
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        cards.push(message)
+        debugger;
+
+    }
+    return cards;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
